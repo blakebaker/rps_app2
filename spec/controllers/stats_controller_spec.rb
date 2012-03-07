29060a1b39ea4fdf90afaeaa78b1c63976568aa2
BBakerpkt <bbakerpkt@gmail.com>
2012-03-07 12:43:36 -0800

    Modifying controllers

--- spec/controllers/stats_controller_spec.rb
+++ spec/controllers/stats_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe StatsController do
+  describe "GET 'stats'" do
+    it "should be successful" do
+      get 'stats'
+      response.should be_success
+    end
+  end
+end
 
 end
--- spec/controllers/throw_controller_spec.rb
+++ spec/controllers/throw_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe ThrowController do
-
+  describe "GET 'player_throw'" do
+    it "should be successful" do
+      get 'player_throw'
+      response.should be_success
+    end
+  end
 end
