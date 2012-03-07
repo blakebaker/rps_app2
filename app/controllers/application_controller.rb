f01fedc9d0ff5d850d4d996d9fa29ec09c5fc83f
BBakerpkt <bbakerpkt@gmail.com>
2012-03-07 13:52:20 -0800

    HTML format and some session testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  session :on
-  before_filter :test
-  skip_before_filter :test
-	def test
-		session[:win] = 0
-		session[:loss] = 0
-		session[:tie] = 0
-	end
 end
