e2a8a42b5d115b3f4d837c4cd815dc19b95dfb3f
BBakerpkt <bbakerpkt@gmail.com>
2012-03-07 13:31:54 -0800

    working on making sessions work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  session :on
+  before_filter :test
+  skip_before_filter :test
+	def test
+		session[:win] = 0
+		session[:loss] = 0
+		session[:tie] = 0
+	end
 end
