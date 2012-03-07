82c430c35c43e044aa49736793a228cb05882eba
BBakerpkt <bbakerpkt@gmail.com>
2012-03-07 13:17:51 -0800

    Modifying view

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ RpsApp::Application.routes.draw do
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
   match 'throw/:type' => 'throw#throwing'
-  match 'stats/find' => 'stats#dind'
+  match 'stats/find' => 'stats#find'
 end
